37524bac9b1bf52dde081934e4a31b985cdcf97c
rita <rita.merhej@hotmail.com>
2024-02-25 17:01:58 +0200

    fix/ overflow has the same background as the initial content

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,10 +4,8 @@ import projects from "../data/projects";
 
 const Projects = () => {
   return (
-    <div className="pages-common-style gap-9 h-full">
-      <PageHeading title="Projects" className="text-3xl text-black font-bold" />
-
-      <div className="w-screen h-full bg-gray-300 py-6 justify-items-center grid grid-cols-1 gap-3 lg:grid-cols-2">
+    <div className="pages-common-style gap-9 h-screen bg-gray-300 overflow-visible">
+      <div className="w-screen py-6 justify-items-center grid grid-cols-1 gap-3 lg:grid-cols-2">
         {projects.map((project) => (
           <ProjectCard project={project} />
         ))}
