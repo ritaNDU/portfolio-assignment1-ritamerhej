1696f53df4a522220207c15fd1615edfb8908192
rita <rita.merhej@hotmail.com>
2024-02-26 18:48:34 +0200

    fix/ tried something made a mess then fixed it

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,8 +3,8 @@ import projects from "../data/projects";
 
 const Projects = () => {
   return (
-    <div className="pages-common-style gap-9 h-screen ">
-      <div className="w-screen py-6 justify-items-center grid grid-cols-1 gap-3 lg:grid-cols-2 overflow-y-visible bg-gray-300 ">
+    <div className="pages-common-style gap-9  ">
+      <div className="w-screen py-6 justify-items-center grid grid-cols-1 gap-3 lg:grid-cols-2 h-screen overflow-y-visible bg-gray-300 ">
         {projects.map((project) => (
           <ProjectCard project={project} />
         ))}
